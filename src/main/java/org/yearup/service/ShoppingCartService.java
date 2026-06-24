1068b6a79de2e2c0d302c66b7fdfaf09dc9479d5
colocky <callmeadam.ar@gmail.com>
2026-06-24 10:57:28 -0500

    Fixed clearCart method in ShoppingCartService

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,5 +1,6 @@
 package org.yearup.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.yearup.models.CartItem;
 import org.yearup.models.Product;
@@ -79,6 +80,7 @@ public class ShoppingCartService
         return getByUserId(userId);
     }
 
+    @Transactional
     public ShoppingCart clearCart(int userId) {
         shoppingCartRepository.deleteByUserId(userId);
 
