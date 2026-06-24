b666df96a500a30293f8b1aa232a205374a7ceec
colocky <callmeadam.ar@gmail.com>
2026-06-24 14:12:55 -0500

    Add findByOrderId to OrderLineItemRepository

--- src/main/java/org/yearup/repository/OrderLineItemRepository.java
+++ src/main/java/org/yearup/repository/OrderLineItemRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yearup.models.OrderLineItem;
 
+import java.util.List;
+
 @Repository
 public interface OrderLineItemRepository extends JpaRepository<OrderLineItem, Integer> {
-
+    List<OrderLineItem> findByOrderId(int orderId);
 }
